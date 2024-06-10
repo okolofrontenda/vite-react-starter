479b83b3f096f9eba3ee37768714ed33eedc19de
okolofrontenda <okolofrontenda@gmail.com>
2024-06-10 17:08:36 +0200

    chore: PROJ-11: Конфигурация vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import pluginChecker from 'vite-plugin-checker'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		pluginChecker({
+			typescript: true,
+			eslint: {
+				lintCommand:
+					'eslint . --ext ts,tsx --report-unused-disable-directives --max-warnings 0',
+			},
+		}),
+	],
+	resolve: {
+		alias: [{ find: '@', replacement: '/src' }],
+	},
 })
